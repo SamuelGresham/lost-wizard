89187c2f6fd8024504c09d2eaf71940a89929d63
greshexe <samuelgresham12@gmail.com>
2024-12-19 18:18:54 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,162 @@
+artists = [
+    "Fisher","Marlon Hoffstadt","Tinashe","Sammy Virji","Royel Otis","Caribou","Flight Facilities","AJ Tracey","Confidence Man","Denis Sulta","Ki/Ki","DJ Boring","SG Lewis","Tinlicker","Interplanetary Criminal","Teenage Dads","The Rions","Glass Beams","Pretty Girl","Girls Don't Sync","Oden and Fatzo","Malugi","CC:DISCO!","Neil Frances","Flowdan & Neffa-T","Lola Young","Telenova","Kita Alexander","Sally C","Fish56OctAgon","Moxie","Jennifer Cardini","Chloé Caillet","Little Fritter","Rum Jungle","Nick Ward","Sarah Story","Dameeeela","James Pepper","Don West","Radio Free Alice","Caleb Jackson","Elijah Something","Total Tommy","Krystal Rivvers","Casual Fan","Djanaba","Micah Jey","Micra","Civic Video","Who is Arcadia B2B Half Cut","Surely Shirley","Green Hand Band","Entity","Uncle Ru","Conspiracy Crew","Lost Soundsystem","Disco Dora","c h l o e","MASH","Harry Hooper","Gabriella Spritz","Sasha Milani B2B FUCHSIA","Sim Select","Tokyo Sexwale","DAYZZI B2B DAUG","Cee Vee","OSCILL8","OLIIV","Penelope","Siila","Zach Williams","COZI","Toaka & Mr Roarke"
+]
+
+aidata = [
+    {name:"Fisher", keys:["house","tech house","electronic","dance","club","bass","groove","DJ","producer","festival"], genres:["Tech House"]},
+    {name:"Marlon Hoffstadt", keys:["house","techno","electronic","dance","deep house","DJ","producer","Berlin","underground","groove"], genres:["House","Melodic Techno"]},
+    {name:"Tinashe", keys:["R&B","pop","hip-hop","singer-songwriter","contemporary R&B","dance-pop","alternative R&B","performer","actress","producer"], genres:["R&B","Pop"]},
+    {name:"Sammy Virji", keys:["garage","bassline","house","electronic","dance","UK garage","DJ","producer","club","groove"], genres:["UK Garage","Bassline"]},
+    {name:"Royel Otis", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","songwriting","energetic","melodic"], genres:["Indie Pop","Psychedelic Rock"]},
+    {name:"Caribou", keys:["electronic","indie pop","psychedelic pop","dance music","experimental","producer","DJ","live performance","melodic","ambient"], genres:["Electronic","Indietronica"]},
+    {name:"Flight Facilities", keys:["electronic","indie dance","house","nu-disco","DJ duo","producer duo","Australian music","melodic house","vocal collaborations"], genres:["Disco","House"]},
+    {name:"AJ Tracey", keys:["hip-hop","grime","rap","UK rap","lyricist","rapper-songwriter","trap music","drill music"], genres:["British Hip Hop","Grime"]},
+    {name:"Confidence Man", keys:["indie pop","dance-pop","electro-pop","Australian music","band","energetic","live performance","catchy melodies"], genres:["Electropop"]},
+    {name:"Denis Sulta", keys:["house","techno","electronic","dance","DJ","producer","Glasgow","underground","groove"], genres:["House","Techno"]},
+    {name:"Ki/Ki", keys:["techno","trance","electronic","DJ","producer","rave","high-energy","underground","dance","club"], genres:["Techno"]},
+    {name:"DJ Boring", keys:["house","lo-fi house","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House","Lo-fi House"]},
+    {name:"SG Lewis", keys:["electronic","dance","pop","house","singer-songwriter","producer","DJ","melodic","groove","ambient"], genres:["Electronic","Dance"]},
+    {name:"Tinlicker", keys:["progressive house","melodic techno","electronic","dance","DJ duo","producer duo","melodic","atmospheric","groove","club"], genres:["Progressive House","Melodic Techno"]},
+    {name:"Interplanetary Criminal", keys:["garage","bassline","electronic","dance","DJ","producer","UK garage","underground","club","groove"], genres:["UK Garage"]},
+    {name:"Teenage Dads", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"The Rions", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"Glass Beams", keys:["psychedelic","electronic","experimental","ambient","instrumental","producer","atmospheric","groove","melodic","live performance"], genres:["Psychedelic Rock"]},
+    {name:"Pretty Girl", keys:["electronic","dance","pop","house","DJ","producer","melodic","groove","ambient","club"], genres:["Electronic","Ambient"]},
+    {name:"Girls Don't Sync", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House","UK Garage"]},
+    {name:"Oden and Fatzo", keys:["house","tech house","electronic","dance","DJ duo","producer duo","groove","club","bass","underground"], genres:["House","Disco"]},
+    {name:"Malugi", keys:["techno","electronic","dance","DJ","producer","underground","high-energy","rave","club","groove"], genres:["Techno"]},
+    {name:"CC:DISCO!", keys:["disco","house","electronic","dance","DJ","producer","groove","club","funk","soul"], genres:["Disco","House"]},
+    {name:"Neil Frances", keys:["indie pop","electronic","dance","band","melodic","groove","live performance","vocals","songwriting","ambient"], genres:["Indie Pop","Electronic"]},
+    {name:"Flowdan & Neffa-T", keys:["grime","hip-hop","rap","UK rap","DJ","producer","underground","bass","club","high-energy"], genres:["Grime","Dubstep"]},
+    {name:"Lola Young", keys:["R&B","soul","pop","singer-songwriter","contemporary R&B","performer","vocals","melodic","alternative R&B","live performance"], genres:["Pop","Soul"]},
+    {name:"Telenova", keys:["indie pop","electronic","alternative pop","band","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Kita Alexander", keys:["indie pop","pop","singer-songwriter","Australian music","melodic","vocals","live performance","songwriting","alternative pop"], genres:["Pop"]},
+    {name:"Sally C", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Fish56OctAgon", keys:["electronic","experimental","ambient","producer","DJ","groove","melodic","atmospheric","live performance"], genres:["Experimental Electronic"]},
+    {name:"Moxie", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House","Techno"]},
+    {name:"Jennifer Cardini", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Techno","House"]},
+    {name:"Chlo\'e9 Caillet", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House","Disco"]},
+    {name:"Little Fritter", keys:["house","tech house","electronic","dance","DJ","producer","groove","club","bass","underground"], genres:["House"]},
+    {name:"Rum Jungle", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"Nick Ward", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Sarah Story", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Dameeeela", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Electronic","Dance"]},
+    {name:"James Pepper", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Don West", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Indie Pop"]},
+    {name:"Radio Free Alice", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"Caleb Jackson", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Elijah Something", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Indie Rock"]},
+    {name:"Total Tommy", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Electronic"]},
+    {name:"Krystal Rivvers", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Casual Fan", keys:["indie pop","electronic","alternative pop","band","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Djanaba", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["R&B","Pop"]},
+    {name:"Micah Jey", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Pop"]},
+    {name:"Micra", keys:["indie pop","electronic","alternative pop","band","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Civic Video", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"Who is Arcadia B2B Half Cut", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Techno"]},
+    {name:"Surely Shirley", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Indie Pop"]},
+    {name:"Green Hand Band", keys:["indie rock","alternative rock","pop rock","band","guitar-driven","vocals","live performance","energetic","melodic","songwriting"], genres:["Indie Rock"]},
+    {name:"Entity", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Techno"]},
+    {name:"Uncle Ru", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Conspiracy Crew", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Techno"]},
+    {name:"Lost Soundsystem", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Electronic"]},
+    {name:"Disco Dora", keys:["disco","house","electronic","dance","DJ","producer","groove","club","funk","soul"], genres:["Disco"]},
+    {name:"c h l o e", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"MASH", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Harry Hooper", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},
+    {name:"Gabriella Spritz", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},
+    {name:"Sasha Milani B2B FUCHSIA", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Techno"]},
+    {name:"Sim Select", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},{name:"Tokyo Sexwale", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Techno"]},{name:"DAYZZI B2B DAUG", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Techno"]},{name:"Cee Vee", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},{name:"OSCILL8", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Techno"]},{name:"OLIIV", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},{name:"Penelope", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Pop"]},{name:"Siila", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["Indie Pop"]},{name:"Zach Williams", keys:["indie pop","electronic","alternative pop","singer-songwriter","melodic","vocals","live performance","songwriting","ambient"], genres:["Indie Rock"]},{name:"COZI", keys:["house","techno","electronic","dance","DJ","producer","underground","groove","club","bass"], genres:["House"]},{name:"Toaka & Mr Roarke", keys:["house","techno","electronic","dance","DJ duo","producer duo","underground","groove","club","bass"], genres:["Techno"]}
+]
+
+function go() {
+    var liked_artists = []
+    for (i in artists) { 
+        let str = document.getElementById(artists[i]).value
+        let chk = document.getElementById(artists[i]).checked
+
+        if (chk) { 
+            liked_artists.push(str)
+        }
+    }
+
+    console.log(liked_artists)
+
+    liked_artists_data = []
+
+    for (i in aidata) {
+        if (liked_artists.includes(aidata[i].name)) {
+            liked_artists_data.push(aidata[i])
+        }
+    }
+
+    for (i in aidata) {
+        me = aidata[i].name
+        artistkeys = aidata[i].keys
+        artistgenres = aidata[i].genres
+        for (j in liked_artists) {
+            likedartistname = liked_artists_data[j].name
+            likedartistkeys = liked_artists_data[j].keys
+            likedartistgenres = liked_artists_data[j].genres
+
+            keywordMatch = 0
+            GenreMatch = 0
+
+            keysoverlap = artistkeys.filter(value => likedartistkeys.includes(value))
+            genreoverlap = artistgenres.filter(value => likedartistgenres.includes(value))
+
+            aidata[i][liked_artists[j]] = [keysoverlap.length + genreoverlap.length*10]
+        }
+    }
+
+    for (i in aidata) {
+        aidata[i].total_score = 0
+        for (j in liked_artists) {
+            aidata[i].total_score += Number(aidata[i][liked_artists[j]])
+        }
+    }
+
+    aidata.sort((a, b) => b.total_score - a.total_score)
+
+    document.getElementById('results').innerHTML = '';
+    document.getElementById('checks').innerHTML = ""
+
+    let pElement = document.createElement('p');
+    pElement.textContent = "The wizard says..."
+    // Find the div with the id 'results'
+    let resultsDiv = document.getElementById('results');
+
+    // Append the <p> tag to the div
+    resultsDiv.appendChild(pElement);
+
+    for (i in aidata){
+        // Create a new <p> element
+        let pElement = document.createElement('p');
+
+        // Set the content of the <p> tag
+        pElement.textContent = Number(i)+1 + ". " + aidata[i].name + " (" + Math.floor(aidata[i].total_score*100/aidata[0].total_score) + "%)";
+
+        pElement.title = aidata[i].keys
+
+        if (liked_artists.includes(aidata[i].name)) {
+            pElement.style.color = "#AAAAAA"
+        } else {
+            pElement.style.color = getColor(aidata[i].total_score/aidata[0].total_score)
+        }
+
+    
+
+        // Find the div with the id 'results'
+        let resultsDiv = document.getElementById('results');
+
+        // Append the <p> tag to the div
+        resultsDiv.appendChild(pElement);
+    }
+
+}
+
+function getColor(value){
+    //value from 0 to 1
+    var hue=((value)*120).toString(10);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
